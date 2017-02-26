d3d9ddd36e6678dacfce8087bc424181c277624c
Muahah <gaby.launay@insa-lyon.fr>
2017-02-26 13:54:46 +0100

    Add init file to use with zprezto

---
+++ init.zsh
@@ -0,0 +1,13 @@
+#
+# Provides support for zsh-image-extension:
+# https://github.com/Vifon/zsh-image-extension
+#
+# Note:
+#   Need sxiv installed
+#
+# Authors:
+#   Gaby Launay <gaby.launay@tutanota.com>
+#
+
+# source main file
+source ${0:h}/zsh-image-extension.zsh
